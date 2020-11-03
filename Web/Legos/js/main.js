7c81e9f9c35347a924e2a961d483ad37524cc03b
Nurali <71943120+Nurali-verstalshik@users.noreply.github.com>
2020-11-03 23:42:39 +0300

    Add files via upload

---
+++ Web/Legos/js/main.js
@@ -0,0 +1,31 @@
+// плавный подьем
+
+jQuery(document).ready(function() {
+jQuery(".scrollto a").click(function () {
+elementClick = jQuery(this).attr("href")
+destination = jQuery(elementClick).offset().top - 100;
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+return false;
+});
+});
+//конец  плавный подьем
+$(".hover_link").text(""); 
+$(".ar").css("left","0px"); 
+function inter() {
+	$(".price").css('color', '#6E7988');
+	$(".hover_link").text("Подробнее"); 
+	$(".ar").css("left","100px");
+	  
+};
+
+function out() {
+	$(".price").css('color', '#0067B4');
+	$(".hover_link").text(""); 
+	$(".ar").css("left","0px"); 
+};
+
+
+// $(".object").hover(function() {
+// 	$(".object .price").css('color', 'red');
+//  $(this).css('background', 'green');
+// }); 
