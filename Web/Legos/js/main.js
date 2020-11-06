0fcb6be601fea546dab1ee9bda60643421d947d6
Nurali <71943120+Nurali-verstalshik@users.noreply.github.com>
2020-11-06 22:08:46 +0300

    Add files via upload

--- Web/Legos/js/main.js
+++ Web/Legos/js/main.js
@@ -1,14 +1,57 @@
 // плавный подьем
 
-jQuery(document).ready(function() {
-	jQuery(".scrollto a").click(function () {
-		elementClick = jQuery(this).attr("href")
-		destination = jQuery(elementClick).offset().top - 100;
-		jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
-		return false;
+
+// When the user scrolls down 20px from the top of the document, show the button
+$(document).ready(function(){
+
+    $(function (){
+		$("#ToTheTop").hide();
+
+		$(window).scroll(function (){
+			if ($(this).scrollTop() > 700){
+				$("#ToTheTop").fadeIn();
+			} else{
+				$("#ToTheTop").fadeOut();
+			}
+		});
+
+		$("#ToTheTop").click(function (){
+			$("body,html").animate({
+				scrollTop:0
+			}, 800);
+			return false;
+		});
 	});
 });
+
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// 		document.getElementById("ToTheTop").style.display = "block";
+// 	} else {
+// 		document.getElementById("ToTheTop").style.display = "none";
+// 	}
+// }
+
+// function topFunction() {
+// 		jQuery(".scrollto a").click(function () {
+// 			elementClick = jQuery(this).attr("href")
+// 			destination = jQuery(elementClick).offset().top - 100;
+// 			jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+// 			return false;
+// 		});
+ 
+//     // document.body.scrollTop = 0; // For Safari
+//     // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
 //конец  плавный подьем
+
+
+
 $(".hover_link").text(""); 
 $(".ar").css("left","0px"); 
 function inter() {
